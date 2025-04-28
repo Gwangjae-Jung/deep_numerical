26e588e150ae66c9671c633fc21cf4682ce15bec
GJ2_lab <rhkdwo2048@gmail.com>
2025-04-28 14:58:33 +0900

    250428-02
    * Massive moves

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
+import  warnings
 def __getattr__(attr: str) -> object:
-    if attr == "data_generators":
-        from    .   import  data_generators
-        return  data_generators
     if attr == "numerical":
+        warnings.warn(
+            "The submodule 'numerical' is not actively updated, and will be deprecated in the future. Instead, use 'pytorch.torch_numerical' to implement numerical methods.",
+            DeprecationWarning
+        )
         from    .   import  numerical
         return  numerical
     if attr == "pytorch":
