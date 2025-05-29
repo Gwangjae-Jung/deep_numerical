940b369a692d21f990a7d588c5ed75f733f1a942
GJ2_lab <rhkdwo2048@gmail.com>
2025-05-29 10:54:09 +0900

    250529-01
    * `.gitignore`

--- __DEPRECATED__data_generators/generators/__init__.py
+++
@@ -1,2 +0,0 @@
-from    .charged_pendulum_generator     import  generate_data__charged_pendulum
-from    .pendulum_generator             import  generate_data__pendulum
--- __DEPRECATED__numerical_numpy/config/__init__.py
+++
@@ -1,1 +0,0 @@
-from    ._parsers   import *
